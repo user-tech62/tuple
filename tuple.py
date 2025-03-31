b17d1d0ba49d56dd443dbce715aeecb959263958
user-tech62 <nandinimalik2006@gmail.com>
2025-03-31 13:31:02 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,32 @@
+# len()
+my_tuple = (1, 2, 3)
+print(len(my_tuple)) 
+
+# max()
+my_tuple = (1, 2, 3, 4)
+print(max(my_tuple))
+
+# min()
+my_tuple = (1, 2, 3, 4)
+print(min(my_tuple))  
+
+# sum()
+my_tuple = (1, 2, 3, 4)
+print(sum(my_tuple)) 
+
+# tuple()
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple)  
+
+# index()
+my_tuple = (1, 2, 3, 4)
+print(my_tuple.index(3))  
+
+# count()
+my_tuple = (1, 2, 3, 2, 4, 2)
+print(my_tuple.count(2))
+
+# sorted()
+my_tuple = (4, 1, 3, 2)
+print(sorted.my_tuple())
